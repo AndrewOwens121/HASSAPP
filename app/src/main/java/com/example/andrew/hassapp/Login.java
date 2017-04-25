9e2f78621772975383fe71e3b9724f7ad00e600e
AndrewOwens121 <andrew.owens121@gmail.com>
2017-04-25 22:23:00 +0100

    Log In request #7 Tested PHP script externally without error , bug must be in java code

--- app/src/main/java/com/example/andrew/hassapp/Login.java
+++ app/src/main/java/com/example/andrew/hassapp/Login.java
@@ -1,75 +1,77 @@
 package com.example.andrew.hassapp;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import static com.example.andrew.hassapp.R.id.etUsername;
-
 public class Login extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);//Edit to change title text
-        setSupportActionBar(toolbar);
 
         final EditText etUsername = (EditText) findViewById(R.id.etUsername);
         final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        final TextView tvRegisterLink = (TextView) findViewById(R.id.WelcomeMessage);
         final Button bLogin = (Button) findViewById(R.id.bLogin);
 
-        bLogin.setOnClickListener(new View.OnClickListener(){
+
+
+        bLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 final String username = etUsername.getText().toString();
                 final String password = etPassword.getText().toString();
 
+                // Response received from the server
                 Response.Listener<String> responseListener = new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         try {
                             JSONObject jsonResponse = new JSONObject(response);
                             boolean success = jsonResponse.getBoolean("success");
-                            if(success){
 
-                                String businessname = jsonResponse.getString("businessname");
-                                String username = jsonResponse.getString("username");
+                            if (success) {
+                                String name = jsonResponse.getString("name");
+                                int age = jsonResponse.getInt("age");
 
-                                Intent intent = new Intent(Login.this,MainActivity.class);
-                                intent.putExtra("businessname",businessname);
-                                intent.putExtra("username",username);
+                                Intent intent = new Intent(Login.this, MainActivity.class);
+                                intent.putExtra("name", name);
+                                intent.putExtra("username", username);
                                 Login.this.startActivity(intent);
-
-
-                            }else{
+                            } else {
                                 AlertDialog.Builder builder = new AlertDialog.Builder(Login.this);
-                                builder.setMessage("Log-in Un-Successful").setNegativeButton("Retry", null).create().show();
+                                builder.setMessage("Login Failed")
+                                        .setNegativeButton("Retry", null)
+                                        .create()
+                                        .show();
                             }
+
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
                     }
                 };
 
-
-                LoginRequest loginrequest = new LoginRequest(username,password,responseListener);
-
+                LoginRequest loginRequest = new LoginRequest(username, password, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(Login.this);
+                queue.add(loginRequest);
             }
         });
     }
+
 
     public void bReg(View view){
         Intent i = new Intent(this,Register.class);
