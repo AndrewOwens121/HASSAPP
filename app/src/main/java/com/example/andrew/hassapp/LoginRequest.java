e17e2212de72bbe3e03bd418f49bfcafed3674bb
AndrewOwens121 <andrew.owens121@gmail.com>
2017-04-25 21:15:45 +0100

    Log In request #3

--- app/src/main/java/com/example/andrew/hassapp/LoginRequest.java
+++ app/src/main/java/com/example/andrew/hassapp/LoginRequest.java
@@ -3,17 +3,25 @@ package com.example.andrew.hassapp;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
 
+import java.util.HashMap;
 import java.util.Map;
 
-public class LoginRequest {
+public class LoginRequest extends StringRequest {
 
 
     private static final String REGISTER_REQUEST_URL="http://192.168.0.17/WebD/HASSAPP/register.php";
     private Map<String, String> params;
 
-    public RegisterRequest(String businessname,String email,String username,String password , Response.Listener<String> listener) {
+    public LoginRequest(String businessname,String email,String username,String password , Response.Listener<String> listener) {
         super(Request.Method.POST, REGISTER_REQUEST_URL, listener, null);
+
+        params = new HashMap<>();
+        params.put("businessname",businessname);
+        params.put("email",email);
+        params.put("username",username);
+        params.put("password",password);
     }
 
 }
