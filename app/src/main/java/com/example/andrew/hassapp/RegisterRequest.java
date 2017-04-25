f5aed306346066bc645297057812074bd3955c2c
AndrewOwens121 <andrew.owens121@gmail.com>
2017-04-25 21:08:31 +0100

    Register requests #6 , java Code fixed , REGISTER_REQUEST_URL needed to be changed to IPx4 with folder extension

--- app/src/main/java/com/example/andrew/hassapp/RegisterRequest.java
+++ app/src/main/java/com/example/andrew/hassapp/RegisterRequest.java
@@ -9,7 +9,7 @@ import java.util.Map;
 
 public class RegisterRequest extends StringRequest{
 
-    private static final String REGISTER_REQUEST_URL="localhost";
+    private static final String REGISTER_REQUEST_URL="http://192.168.0.17/WebD/HASSAPP/register.php";
     private Map<String, String> params;
 
     public RegisterRequest(String businessname,String email,String username,String password , Response.Listener<String> listener){
