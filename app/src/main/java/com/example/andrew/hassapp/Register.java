c3402e9243cb329562492d71c7b6ac8798865c56
AndrewOwens121 <andrew.owens121@gmail.com>
2017-04-25 17:52:37 +0100

    Register requests #3

--- app/src/main/java/com/example/andrew/hassapp/Register.java
+++ app/src/main/java/com/example/andrew/hassapp/Register.java
@@ -1,13 +1,20 @@
 package com.example.andrew.hassapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.android.volley.Response;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Register extends AppCompatActivity {
 
@@ -29,6 +36,28 @@ public class Register extends AppCompatActivity {
                 final String businessname = regBusinessName.getText().toString();
                 final String username = regUserName.getText().toString();
                 final String password = regPassword.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>(){
+
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+
+                            if(success){
+                                Intent intent = new Intent(Register.this,Login.class);
+                                Register.this.startActivity(intent);
+                            }else{
+                                AlertDialog.Builder builder = new AlertDialog.Builder(Register.this)
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+
+                RegisterRequest registerRequest = new RegisterRequest(businessname,username,password,)
             }
         });
 
