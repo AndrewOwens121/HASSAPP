51ac30531a432282a675817e99d976fcdfbdc25c
AndrewOwens121 <andrew.owens121@gmail.com>
2017-04-26 15:29:10 +0100

    download button working

--- app/src/main/java/com/example/andrew/hassapp/Tree8.java
+++ app/src/main/java/com/example/andrew/hassapp/Tree8.java
@@ -1,5 +1,8 @@
 package com.example.andrew.hassapp;
 
+import android.app.DownloadManager;
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
@@ -7,11 +10,14 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.VideoView;
 import android.widget.MediaController;
 
 public class Tree8 extends AppCompatActivity {
 
+    DownloadManager downloadManager;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +32,19 @@ public class Tree8 extends AppCompatActivity {
         tree8video.setMediaController(media);
         //starts video
         tree8video.start();
+
+        //Button Click
+        button = (Button)findViewById(R.id.button3);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                downloadManager= (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+                Uri uri=Uri.parse("http://192.168.0.17/WebD/HASSAPP/files/tree8.pdf");
+                DownloadManager.Request request=new DownloadManager.Request(uri);
+                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                Long reference=downloadManager.enqueue(request);
+            }
+        });
 
     }
 
--- app/src/main/java/com/example/andrew/hassapp/Tree9.java
+++ app/src/main/java/com/example/andrew/hassapp/Tree9.java
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class Tree9 extends AppCompatActivity {
 
@@ -14,6 +16,15 @@ public class Tree9 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tree9);
 
+
+        final VideoView tree8video = (VideoView) findViewById(R.id.tree8video);
+        tree8video.setVideoPath("http://192.168.0.17/WebD/HASSAPP/video/tree9.mp4");
+        //Player Controls - play -  pause
+        MediaController media = new MediaController(this);
+        media.setAnchorView(tree8video);
+        tree8video.setMediaController(media);
+        //starts video
+        tree8video.start();
     }
 
 }
