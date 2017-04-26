9535491c56ac4bc365ae43442f8e382c1f785890
AndrewOwens121 <andrew.owens121@gmail.com>
2017-04-26 14:42:28 +0100

    HASSAPP listview started

--- app/src/main/java/com/example/andrew/hassapp/HACCPLIST.java
+++ app/src/main/java/com/example/andrew/hassapp/HACCPLIST.java
@@ -6,13 +6,38 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class HACCPLIST extends AppCompatActivity {
+public class HACCPLIST extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_haccplist);
+
+        String[] manuals = {"Butchers","Deli/Restaurant","Bar/Pub","Smoothie Shop"};
+        ListAdapter adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,manuals);
+        ListView listView = (ListView) findViewById(R.id.ListView);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        String food = String.valueOf(parent.getItemAtPosition(position));
+                    }
+                }
+
+
+        );
+        }
     }
 
-}
+
